80be0e71389c04154448c027ace1f570c45af1f1
George Kexxti <kexxti@DESKTOP-DP79Q9K.>
2025-01-08 01:54:20 +0700

    strlen strpbrk upper lower strncmp

--- src/string/s21_strlen.c
+++ src/string/s21_strlen.c
@@ -4,5 +4,5 @@ s21_size_t s21_strlen(const char *str)
 {
     s21_size_t len = 0;
     for(; str[len]; len++);
-    return len + 1;
+    return len;
 } 
---
+++ src/string/s21_strncmp.c
@@ -0,0 +1,15 @@
+#include "s21_string.h"
+
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n){
+    unsigned char ch1 = '\0';
+    unsigned char ch2 = '\0';
+    while (n > 0) {    
+        ch1 = (unsigned char) *str1++;
+        ch2 = (unsigned char) *str2++;
+        if (ch1 == '\0' || ch1 != ch2){
+            return ch1 - ch2;
+        }
+        n--;
+    }
+    return ch1 - ch2;
+}
