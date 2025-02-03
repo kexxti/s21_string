d93ded0e8933346776202ca3468618328a033225
Denis <denmurashkin@gmail.com>
2025-02-03 20:56:39 +0700

    fix memchr

--- src/string/s21_memchr.c
+++ src/string/s21_memchr.c
@@ -1,16 +1,13 @@
 #include "s21_string.h"
 
 void* s21_memchr(const void* str, int c, s21_size_t n) {
-  s21_size_t len = 0;
+  s21_size_t i = 0;
   unsigned char* mem = (unsigned char*)str;
   void* result = S21_NULL;
-  if (n != 0) {
-    while (*mem != (unsigned char)c && len < n) {
-      len++;
-      mem++;
-    }
-    if (*mem == (unsigned char)c) {
-      result = mem;
+  for (i = 0; i < n; i++) {
+    if (mem[i] == (unsigned char)c) {
+      result = mem + i;
+      break;
     }
   }
   return result;
