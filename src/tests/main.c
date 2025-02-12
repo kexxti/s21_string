bdf49e13520ab83122fd1c86d6d7850e8b5378e2
Денис Мурашкин <denis@Noutbuk-Denis.local>
2025-02-12 19:17:27 +0700

    test_s21_sprintf

--- src/tests/main.c
+++ src/tests/main.c
@@ -7,6 +7,7 @@
 #include "test_s21_memcmp.h"
 #include "test_s21_memcpy.h"
 #include "test_s21_memset.h"
+#include "test_s21_sprintf.h"
 #include "test_s21_sscanf.h"
 #include "test_s21_strchr.h"
 #include "test_s21_strcspn.h"
@@ -39,6 +40,7 @@ int main() {
   srunner_add_suite(sr, s21_memset_suite());
   srunner_add_suite(sr, s21_sscanf_suite());
   srunner_add_suite(sr, s21_strerror_suite());
+  srunner_add_suite(sr, s21_sprintf_suite());
 
   srunner_run_all(sr, CK_NORMAL);
   number_failed = srunner_ntests_failed(sr);
---
+++ src/tests/test_s21_sprintf.h
@@ -0,0 +1,11 @@
+#ifndef TEST_S21_SPRINTF_H
+#define TEST_S21_SPRINTF_H
+
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Suite *s21_sprintf_suite(void);
+
+#endif
