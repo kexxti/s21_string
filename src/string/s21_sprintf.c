067d4789dd3173bd566b11b7f71ec436186b4f16
RealIlya <hairais@mail.ru>
2025-02-13 21:05:53 +0700

    added processing of -,+,space and some features

--- src/string/s21_sprintf.c
+++ src/string/s21_sprintf.c
@@ -16,18 +16,18 @@ int s21_sprintf(char *str, const char *format, ...) {
       *dest++ = *format++;
       written++;
     } else {
-      format++; // пропускаем '%'
+      format++;  // пропускаем '%'
       if (*format == '%') {
         *dest++ = '%';
         written++;
         format++;
         continue;
       }
-      current_format *cf = init_format();
-      fill_format_default(cf);
-      read_format(&format, cf, args);
-      normalize_format(cf);
-      apply_format(&dest, cf, args, &written);
+      current_format *cf = s21_sprintf_init_format();
+      s21_sprintf_fill_format_default(cf);
+      s21_sprintf_read_format(&format, cf, args);
+      s21_sprintf_normalize_format(cf);
+      s21_sprintf_apply_format(&dest, cf, args, &written);
       free(cf);
     }
   }
@@ -37,15 +37,14 @@ int s21_sprintf(char *str, const char *format, ...) {
 }
 
 // Выделение памяти для current_format
-current_format *init_format() {
+current_format *s21_sprintf_init_format() {
   current_format *cf = (current_format *)malloc(sizeof(current_format));
-  if (cf)
-    fill_format_default(cf);
+  if (cf) s21_sprintf_fill_format_default(cf);
   return cf;
 }
 
 // Инициализация значений по умолчанию
-void fill_format_default(current_format *cf) {
+void s21_sprintf_fill_format_default(current_format *cf) {
   cf->is_symbol = false;
   cf->matching_symbol = '\0';
   cf->left_align = false;
@@ -62,33 +61,33 @@ void fill_format_default(current_format *cf) {
 }
 
 // Вспомогательная функция для разбора флагов
-void parse_flags(const char **format, current_format *cf) {
+void s21_sprintf_parse_flags(const char **format, current_format *cf) {
   while (**format) {
     switch (**format) {
-    case '-':
-      cf->left_align = true;
-      break;
-    case '+':
-      cf->force_sign = true;
-      break;
-    case ' ':
-      cf->space = true;
-      break;
-    case '#':
-      cf->alt_form = true;
-      break;
-    case '0':
-      cf->zero_pad = true;
-      break;
-    default:
-      return;
+      case '-':
+        cf->left_align = true;
+        break;
+      case '+':
+        cf->force_sign = true;
+        break;
+      case ' ':
+        cf->space = true;
+        break;
+      case '#':
+        cf->alt_form = true;
+        break;
+      case '0':
+        cf->zero_pad = true;
+        break;
+      default:
+        return;
     }
     (*format)++;
   }
 }
 
 // Получение ширины поля
-s21_size_t get_width(const char **format, va_list args) {
+s21_size_t s21_sprintf_get_width(const char **format, va_list args) {
   s21_size_t width = 0;
   if (**format == '*') {
     width = (s21_size_t)va_arg(args, int);
@@ -103,7 +102,7 @@ s21_size_t get_width(const char **format, va_list args) {
 }
 
 // Получение точности
-s21_size_t get_precision(const char **format, va_list args) {
+s21_size_t s21_sprintf_get_precision(const char **format, va_list args) {
   s21_size_t precision = 0;
   if (**format == '*') {
     precision = (s21_size_t)va_arg(args, int);
@@ -118,13 +117,14 @@ s21_size_t get_precision(const char **format, va_list args) {
 }
 
 // Разбор спецификатора формата
-void read_format(const char **format, current_format *cf, va_list args) {
-  parse_flags(format, cf);
-  cf->width = get_width(format, args);
+void s21_sprintf_read_format(const char **format, current_format *cf,
+                             va_list args) {
+  s21_sprintf_parse_flags(format, cf);
+  cf->width = s21_sprintf_get_width(format, args);
   if (**format == '.') {
     (*format)++;
     cf->precision_specified = true;
-    cf->precision = get_precision(format, args);
+    cf->precision = s21_sprintf_get_precision(format, args);
   }
   if (**format == 'h' || **format == 'l' || **format == 'L') {
     cf->length_modifier =
@@ -133,49 +133,48 @@ void read_format(const char **format, current_format *cf, va_list args) {
   }
   cf->spec = **format;
   switch (cf->spec) {
-  case 'c':
-    cf->type_modifier = CHAR;
-    break;
-  case 's':
-    cf->type_modifier = STRING;
-    break;
-  case 'd':
-  case 'i':
-    cf->type_modifier = DECIMAL;
-    break;
-  case 'u':
-    cf->type_modifier = UDECIMAL;
-    break;
-  case 'o':
-    cf->type_modifier = UOCTAL;
-    break;
-  case 'x':
-  case 'X':
-    cf->type_modifier = UHEX;
-    break;
-  case 'p':
-    cf->type_modifier = POINTER;
-    break;
-  case 'n':
-    cf->type_modifier = NUMBER;
-    break;
-  case 'f':
-  case 'e':
-  case 'E':
-  case 'g':
-  case 'G':
-    cf->type_modifier = FLOAT;
-    break;
-  default:
-    cf->type_modifier = ERR;
-    break;
+    case 'c':
+      cf->type_modifier = CHAR;
+      break;
+    case 's':
+      cf->type_modifier = STRING;
+      break;
+    case 'd':
+    case 'i':
+      cf->type_modifier = DECIMAL;
+      break;
+    case 'u':
+      cf->type_modifier = UDECIMAL;
+      break;
+    case 'o':
+      cf->type_modifier = UOCTAL;
+      break;
+    case 'x':
+    case 'X':
+      cf->type_modifier = UHEX;
+      break;
+    case 'p':
+      cf->type_modifier = POINTER;
+      break;
+    case 'n':
+      cf->type_modifier = NUMBER;
+      break;
+    case 'f':
+    case 'e':
+    case 'E':
+    case 'g':
+    case 'G':
+      cf->type_modifier = FLOAT;
+      break;
+    default:
+      cf->type_modifier = ERR;
+      break;
   }
-  if (**format)
-    (*format)++;
+  if (**format) (*format)++;
 }
 
 // Приведение формата к корректному виду для спецификатора '%'
-void normalize_format(current_format *cf) {
+void s21_sprintf_normalize_format(current_format *cf) {
   if (cf->type_modifier == PERCENT) {
     cf->is_symbol = true;
     cf->matching_symbol = '%';
@@ -183,7 +182,8 @@ void normalize_format(current_format *cf) {
 }
 
 // Запись отступов (padding)
-void write_padding(char **dest, int pad, char pad_char, int *written) {
+void s21_sprintf_write_padding(char **dest, int pad, char pad_char,
+                               int *written) {
   for (int i = 0; i < pad; i++) {
     **dest = pad_char;
     (*dest)++;
@@ -192,7 +192,7 @@ void write_padding(char **dest, int pad, char pad_char, int *written) {
 }
 
 // Вспомогательная функция для разворота строки
-void reverse_str(char *str, int len) {
+void s21_sprintf_reverse_str(char *str, int len) {
   int i = 0, j = len - 1;
   while (i < j) {
     char tmp = str[i];
@@ -204,7 +204,8 @@ void reverse_str(char *str, int len) {
 }
 
 // Преобразование знакового целого в строку
-int itoa_custom(long long num, char *buf, int base, bool uppercase) {
+int s21_sprintf_itoa_custom(long long num, char *buf, int base,
+                            bool uppercase) {
   int i = 0;
   bool neg = false;
   if (num < 0 && base == 10) {
@@ -220,16 +221,16 @@ int itoa_custom(long long num, char *buf, int base, bool uppercase) {
           (rem < 10) ? ('0' + rem) : ((uppercase ? 'A' : 'a') + rem - 10);
       num /= base;
     }
-    if (neg)
-      buf[i++] = '-';
+    if (neg) buf[i++] = '-';
   }
-  reverse_str(buf, i);
+  s21_sprintf_reverse_str(buf, i);
   buf[i] = '\0';
   return i;
 }
 
 // Преобразование беззнакового целого в строку
-int utoa_custom(unsigned long long num, char *buf, int base, bool uppercase) {
+int s21_sprintf_utoa_custom(unsigned long long num, char *buf, int base,
+                            bool uppercase) {
   int i = 0;
   if (num == 0) {
     buf[i++] = '0';
@@ -241,13 +242,13 @@ int utoa_custom(unsigned long long num, char *buf, int base, bool uppercase) {
       num /= base;
     }
   }
-  reverse_str(buf, i);
+  s21_sprintf_reverse_str(buf, i);
   buf[i] = '\0';
   return i;
 }
 
 // Форматирование символа (%c)
-int format_char(va_list args, char *buffer) {
+int s21_sprintf_format_char(va_list args, char *buffer) {
   char ch = (char)va_arg(args, int);
   buffer[0] = ch;
   buffer[1] = '\0';
@@ -255,10 +256,9 @@ int format_char(va_list args, char *buffer) {
 }
 
 // Форматирование строки (%s)
-int format_string(current_format *cf, va_list args, char *buffer) {
+int s21_sprintf_format_string(current_format *cf, va_list args, char *buffer) {
   char *s = va_arg(args, char *);
-  if (!s)
-    s = "(null)";
+  if (!s) s = "(null)";
   int i = 0;
   while (s[i] && (!cf->precision_specified || i < (int)cf->precision)) {
     buffer[i] = s[i];
@@ -269,7 +269,7 @@ int format_string(current_format *cf, va_list args, char *buffer) {
 }
 
 // Форматирование знакового целого (%d/%i)
-int format_decimal(current_format *cf, va_list args, char *buffer) {
+int s21_sprintf_format_decimal(current_format *cf, va_list args, char *buffer) {
   long long num;
   if (cf->length_modifier == LONG)
     num = va_arg(args, long);
@@ -277,12 +277,23 @@ int format_decimal(current_format *cf, va_list args, char *buffer) {
     num = (short)va_arg(args, int);
   else
     num = va_arg(args, int);
-  return itoa_custom(num, buffer, 10, false);
+  int len = s21_sprintf_itoa_custom(num, buffer, 10, false);
+  // Если число положительное, добавляем знак или пробел по флагам
+  if (num >= 0) {
+    if (cf->force_sign || cf->space) {
+      char sign = cf->force_sign ? '+' : ' ';
+      // сдвигаем строку вправо на 1 символ
+      for (int i = len; i >= 0; i--) buffer[i + 1] = buffer[i];
+      buffer[0] = sign;
+      len++;
+    }
+  }
+  return len;
 }
 
 // Форматирование беззнакового целого (%u, %o, %x/%X)
-int format_unsigned(current_format *cf, va_list args, char *buffer, int base,
-                    bool uppercase) {
+int s21_sprintf_format_unsigned(current_format *cf, va_list args, char *buffer,
+                                int base, bool uppercase) {
   unsigned long long num;
   if (cf->length_modifier == LONG)
     num = va_arg(args, unsigned long);
@@ -290,37 +301,66 @@ int format_unsigned(current_format *cf, va_list args, char *buffer, int base,
     num = (unsigned short)va_arg(args, unsigned int);
   else
     num = va_arg(args, unsigned int);
-  return utoa_custom(num, buffer, base, uppercase);
+  int len = s21_sprintf_utoa_custom(num, buffer, base, uppercase);
+  // Обработка альтернативной формы для восьмеричных и шестнадцатеричных чисел
+  if (cf->alt_form) {
+    if (base == 8 && buffer[0] != '0') {
+      for (int i = len; i >= 0; i--) buffer[i + 1] = buffer[i];
+      buffer[0] = '0';
+      len++;
+    } else if (base == 16 && num != 0) {
+      for (int i = len; i >= 0; i--) buffer[i + 2] = buffer[i];
+      buffer[0] = '0';
+      buffer[1] = (uppercase ? 'X' : 'x');
+      len += 2;
+    }
+  }
+  return len;
 }
 
 // Форматирование чисел с плавающей точкой - стандартное представление (%f)
-int format_float(current_format *cf, va_list args, char *buffer) {
+int s21_sprintf_format_float(current_format *cf, va_list args, char *buffer) {
   double val = va_arg(args, double);
   int prec = cf->precision_specified ? cf->precision : 6;
+  int pos = 0;
+  double abs_val = (val < 0) ? -val : val;
+  // Обработка знака
+  if (val < 0) {
+    buffer[pos++] = '-';
+  } else if (cf->force_sign) {
+    buffer[pos++] = '+';
+  } else if (cf->space) {
+    buffer[pos++] = ' ';
+  }
   if (cf->spec == 'f') {
-    double abs_val = (val < 0) ? -val : val;
     long long int_part = (long long)abs_val;
     double frac = abs_val - int_part;
-    int pos = itoa_custom(int_part, buffer, 10, false);
-    buffer[pos++] = '.';
-    for (int i = 0; i < prec; i++) {
-      frac *= 10;
-      int d = (int)frac;
-      buffer[pos++] = '0' + d;
-      frac -= d;
+    pos += s21_sprintf_itoa_custom(int_part, buffer + pos, 10, false);
+    // Если задана точность или alt_form, обязательно выводим десятичную точку
+    if (cf->precision_specified ? (prec > 0 || cf->alt_form) : 1) {
+      buffer[pos++] = '.';
+      if (prec > 0) {
+        for (int i = 0; i < prec; i++) {
+          frac *= 10;
+          int d = (int)frac;
+          buffer[pos++] = '0' + d;
+          frac -= d;
+        }
+      }
     }
     buffer[pos] = '\0';
     return pos;
   } else if (cf->spec == 'e' || cf->spec == 'E' || cf->spec == 'g' ||
              cf->spec == 'G') {
-    return format_float_sci(val, prec, buffer,
-                            (cf->spec == 'E' || cf->spec == 'G'));
+    return s21_sprintf_format_float_sci(val, prec, buffer,
+                                        (cf->spec == 'E' || cf->spec == 'G'));
   }
   return 0;
 }
 
 // Форматирование чисел с плавающей точкой в научной нотации (%e/%E/%g/%G)
-int format_float_sci(double val, int prec, char *buffer, bool uppercase) {
+int s21_sprintf_format_float_sci(double val, int prec, char *buffer,
+                                 bool uppercase) {
   int pos = 0;
   if (val < 0) {
     buffer[pos++] = '-';
@@ -338,7 +378,7 @@ int format_float_sci(double val, int prec, char *buffer, bool uppercase) {
       exponent--;
     }
   }
-  pos += itoa_custom((long long)norm, buffer + pos, 10, false);
+  pos += s21_sprintf_itoa_custom((long long)norm, buffer + pos, 10, false);
   buffer[pos++] = '.';
   double frac = norm - (long long)norm;
   for (int i = 0; i < prec; i++) {
@@ -354,81 +394,78 @@ int format_float_sci(double val, int prec, char *buffer, bool uppercase) {
   } else {
     buffer[pos++] = '+';
   }
-  if (exponent < 10) {
-    buffer[pos++] = '0';
-  }
-  pos += itoa_custom(exponent, buffer + pos, 10, false);
+  if (exponent < 10) buffer[pos++] = '0';
+  pos += s21_sprintf_itoa_custom(exponent, buffer + pos, 10, false);
   buffer[pos] = '\0';
   return pos;
 }
 
 // Форматирование указателя (%p)
-int format_pointer(va_list args, char *buffer) {
+int s21_sprintf_format_pointer(va_list args, char *buffer) {
   void *ptr = va_arg(args, void *);
   unsigned long long addr = (unsigned long long)ptr;
   int pos = 0;
   buffer[pos++] = '0';
   buffer[pos++] = 'x';
-  pos += utoa_custom(addr, buffer + pos, 16, false);
+  pos += s21_sprintf_utoa_custom(addr, buffer + pos, 16, false);
   return pos;
 }
 
 // Функция форматирования: выбирает нужный обработчик, вычисляет padding и
 // записывает результат
-void apply_format(char **dest, current_format *cf, va_list args, int *written) {
+void s21_sprintf_apply_format(char **dest, current_format *cf, va_list args,
+                              int *written) {
   char temp[1024] = {0};
   int len = 0;
   switch (cf->type_modifier) {
-  case CHAR:
-    len = format_char(args, temp);
-    break;
-  case STRING:
-    len = format_string(cf, args, temp);
-    break;
-  case DECIMAL:
-  case INTEGER:
-    len = format_decimal(cf, args, temp);
-    break;
-  case UDECIMAL:
-    len = format_unsigned(cf, args, temp, 10, false);
-    break;
-  case UOCTAL:
-    len = format_unsigned(cf, args, temp, 8, false);
-    break;
-  case UHEX:
-    bool up = (cf->spec == 'X');
-    len = format_unsigned(cf, args, temp, 16, up);
-    break;
-  case POINTER:
-    len = format_pointer(args, temp);
-    break;
-  case FLOAT:
-    len = format_float(cf, args, temp);
-    break;
-  case NUMBER:
-    int *n_ptr = va_arg(args, int *);
-    if (n_ptr)
-      *n_ptr = *written;
-    return;
-  case PERCENT:
-    temp[0] = '%';
-    temp[1] = '\0';
-    len = 1;
-    break;
-  default:
-    temp[0] = cf->spec;
-    temp[1] = '\0';
-    len = 1;
-    break;
+    case CHAR:
+      len = s21_sprintf_format_char(args, temp);
+      break;
+    case STRING:
+      len = s21_sprintf_format_string(cf, args, temp);
+      break;
+    case DECIMAL:
+    case INTEGER:
+      len = s21_sprintf_format_decimal(cf, args, temp);
+      break;
+    case UDECIMAL:
+      len = s21_sprintf_format_unsigned(cf, args, temp, 10, false);
+      break;
+    case UOCTAL:
+      len = s21_sprintf_format_unsigned(cf, args, temp, 8, false);
+      break;
+    case UHEX:
+      bool up = (cf->spec == 'X');
+      len = s21_sprintf_format_unsigned(cf, args, temp, 16, up);
+      break;
+    case POINTER:
+      len = s21_sprintf_format_pointer(args, temp);
+      break;
+    case FLOAT:
+      len = s21_sprintf_format_float(cf, args, temp);
+      break;
+    case NUMBER:
+      int *n_ptr = va_arg(args, int *);
+      if (n_ptr) *n_ptr = *written;
+      return;
+    case PERCENT:
+      temp[0] = '%';
+      temp[1] = '\0';
+      len = 1;
+      break;
+    default:
+      temp[0] = cf->spec;
+      temp[1] = '\0';
+      len = 1;
+      break;
   }
   int pad = (cf->width > (s21_size_t)len) ? cf->width - len : 0;
   if (!cf->left_align)
-    write_padding(dest, pad,
-                  (cf->zero_pad && !cf->precision_specified) ? '0' : ' ',
-                  written);
+    s21_sprintf_write_padding(
+        dest, pad, (cf->zero_pad && !cf->precision_specified) ? '0' : ' ',
+        written);
   s21_memcpy(*dest, temp, len);
   *dest += len;
   *written += len;
-  if (cf->left_align)
-    write_padding(dest, pad, ' ', written);
+  if (cf->left_align) s21_sprintf_write_padding(dest, pad, ' ', written);
 }
--- src/string/s21_sprintf.h
+++ src/string/s21_sprintf.h
@@ -25,42 +25,47 @@ typedef enum LENGTH { NONE, SHORT, LONG, EXTENDED_DOUBLE } LENGTH;
 
 // Структура current_format (имя оставлено, как в s21_sscanf)
 typedef struct current_format {
-  bool is_symbol; // Если нет спецификатора - просто символ для вывода
-  char matching_symbol; // Если is_symbol==true, этот символ выводится
+  bool is_symbol;        // Если нет спецификатора - просто символ для вывода
+  char matching_symbol;  // Если is_symbol==true, этот символ выводится
 
-  bool left_align; // Флаг '-'
-  bool force_sign; // Флаг '+'
-  bool space;      // Флаг ' ' (пробел)
-  bool alt_form;   // Флаг '#'
-  bool zero_pad;   // Флаг '0'
+  bool left_align;  // Флаг '-'
+  bool force_sign;  // Флаг '+'
+  bool space;       // Флаг ' ' (пробел)
+  bool alt_form;    // Флаг '#'
+  bool zero_pad;    // Флаг '0'
 
-  s21_size_t width; // Минимальная ширина поля
+  s21_size_t width;  // Минимальная ширина поля
   bool precision_specified;
-  s21_size_t precision; // Значение точности
+  s21_size_t precision;  // Значение точности
 
-  LENGTH length_modifier; // Модификатор длины (h, l, L)
-  MODIFIER type_modifier; // Тип спецификатора
-  char spec; // Фактический символ спецификатора (например, 'f', 'e', 'g', etc.)
+  LENGTH length_modifier;  // Модификатор длины (h, l, L)
+  MODIFIER type_modifier;  // Тип спецификатора
+  char
+      spec;  // Фактический символ спецификатора (например, 'f', 'e', 'g', etc.)
 } current_format;
 
 int s21_sprintf(char *str, const char *format, ...);
-current_format *init_format();
-void fill_format_default(current_format *cf);
-void read_format(const char **format, current_format *cf, va_list args);
-s21_size_t get_width(const char **format, va_list args);
-s21_size_t get_precision(const char **format, va_list args);
-void normalize_format(current_format *cf);
-void apply_format(char **dest, current_format *cf, va_list args, int *written);
+current_format *s21_sprintf_init_format();
+void s21_sprintf_fill_format_default(current_format *cf);
+void s21_sprintf_read_format(const char **format, current_format *cf,
+                             va_list args);
+s21_size_t s21_sprintf_get_width(const char **format, va_list args);
+s21_size_t s21_sprintf_get_precision(const char **format, va_list args);
+void s21_sprintf_normalize_format(current_format *cf);
+void s21_sprintf_apply_format(char **dest, current_format *cf, va_list args,
+                              int *written);
 
 /* Прототипы вспомогательных функций форматирования */
-int format_char(va_list args, char *buffer);
-int format_string(current_format *cf, va_list args, char *buffer);
-int format_decimal(current_format *cf, va_list args, char *buffer);
-int format_unsigned(current_format *cf, va_list args, char *buffer, int base,
-                    bool uppercase);
-int format_float(current_format *cf, va_list args, char *buffer);
-int format_float_sci(double val, int prec, char *buffer, bool uppercase);
-int format_pointer(va_list args, char *buffer);
-void write_padding(char **dest, int pad, char pad_char, int *written);
+int s21_sprintf_format_char(va_list args, char *buffer);
+int s21_sprintf_format_string(current_format *cf, va_list args, char *buffer);
+int s21_sprintf_format_decimal(current_format *cf, va_list args, char *buffer);
+int s21_sprintf_format_unsigned(current_format *cf, va_list args, char *buffer,
+                                int base, bool uppercase);
+int s21_sprintf_format_float(current_format *cf, va_list args, char *buffer);
+int s21_sprintf_format_float_sci(double val, int prec, char *buffer,
+                                 bool uppercase);
+int s21_sprintf_format_pointer(va_list args, char *buffer);
+void s21_sprintf_write_padding(char **dest, int pad, char pad_char,
+                               int *written);
 
-#endif // S21_SPRINTF_H
+#endif  // S21_SPRINTF_H
